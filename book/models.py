3cf893d49064094469d8b040b400d68c0eef0275
hinelmistry21 <mistryhinel8173@gmail.com>
2020-04-22 18:02:44 +0530

    Library with django

---
+++ book/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+#DataFlair Models
+class Book(models.Model):
+    name = models.CharField(max_length = 50)
+    picture = models.ImageField()
+    author = models.CharField(max_length = 30)
+    email = models.EmailField(blank = True)
+    describe = models.TextField(default = 'Please Enter the Description of the Book in Brief')
+    def __str__(self):
+        return self.name
+
